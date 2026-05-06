d05c04aaa708d62586efba73e682cb8b771c9a0f
Parth Gupta <guptaparth2209@gmail.com>
2026-05-06 12:09:03 +0530

    manifesto section created

---
+++ site/src/ui/Manifesto.jsx
@@ -0,0 +1,14 @@
+import './Manifesto.css';
+
+export default function Manifesto() {
+  return (
+    <section className="manifesto">
+      <div className="manifesto__content">
+        <h2 className="manifesto__text">
+          WE DIDN'T BUILD A VISUALIZER.<br />
+          WE BUILT A GALLERY FOR YOUR AUDIO.
+        </h2>
+      </div>
+    </section>
+  );
+}
